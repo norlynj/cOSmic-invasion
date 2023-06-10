84a064d6a411385388517ae6a5eb40eb26133845
norlynj <nacastillo2@up.edu.ph>
2023-06-10 14:13:49 +0800

    update scrollbar color

--- src/com/view/component/ScrollBar.java
+++ src/com/view/component/ScrollBar.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class ScrollBar extends JScrollBar {
 
-    private Color theme = new Color(115, 200, 177);
+    private Color theme = Color.gray;
 
     public ScrollBar() {
         setUI(new ModernScrollBarUI());

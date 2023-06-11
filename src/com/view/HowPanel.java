29090f310695a83390408c3a7a373b468b791790
norlynj <nacastillo2@up.edu.ph>
2023-06-11 17:37:27 +0800

    fix how panel

--- src/com/view/HowPanel.java
+++ src/com/view/HowPanel.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class HowPanel extends Panel{
 
-    private ImageButton musicOnButton, musicOffButton, homeButton, entitiesButton, controlsButton, levelsButton;
+    private ImageButton musicOnButton, musicOffButton, homeButton, entitiesButton, controlsButton, levelsButton, entitiesAButton, controlsAButton, levelsAButton;
     public HowPanel() {
 
         super("bg/how-1.png");
@@ -20,6 +20,15 @@ public class HowPanel extends Panel{
         controlsButton = new ImageButton("buttons/controls.png");
         levelsButton = new ImageButton("buttons/levels.png");
 
+        // active state
+        entitiesAButton = new ImageButton("buttons/entities-hover.png");
+        controlsAButton = new ImageButton("buttons/controls-hover.png");
+        levelsAButton = new ImageButton("buttons/levels-hover.png");
+
+        entitiesAButton.setVisible(false);
+        controlsAButton.setVisible(false);
+        levelsAButton.setVisible(false);
+
         musicOffButton.setVisible(false);
 
         setListeners();
@@ -30,6 +39,9 @@ public class HowPanel extends Panel{
         this.add(entitiesButton);
         this.add(controlsButton);
         this.add(levelsButton);
+        this.add(entitiesAButton);
+        this.add(controlsAButton);
+        this.add(levelsAButton);
     }
 
     @Override
@@ -39,9 +51,7 @@ public class HowPanel extends Panel{
         musicOnButton.setBounds(939, 22, 40, 54);
         musicOffButton.setBounds(939, 22, 40, 54);
         homeButton.setBounds(993, 30, 40, 42);
-        entitiesButton.setBounds(330, 126, 133, 41);
-        controlsButton.setBounds(486, 126, 150, 41);
-        levelsButton.setBounds(655, 126, 121, 41);
+        setButtonBounds();
 
     }
 
@@ -52,37 +62,47 @@ public class HowPanel extends Panel{
         entitiesButton.hover("buttons/entities-hover.png", "buttons/entities.png");
         controlsButton.hover("buttons/controls-hover.png", "buttons/controls.png");
         levelsButton.hover("buttons/levels-hover.png", "buttons/levels.png");
+        entitiesAButton.hover("buttons/entities.png", "buttons/entities-hover.png");
+        controlsAButton.hover("buttons/controls.png", "buttons/controls-hover.png");
+        levelsAButton.hover("buttons/levels.png", "buttons/levels-hover.png");
         listenToButtonClicks();
     }
 
     private void listenToButtonClicks() {
         entitiesButton.addActionListener(e -> {
+            setButtonActiveState(entitiesAButton, entitiesButton);
+            setButtonActiveState(controlsButton, controlsAButton);
+            setButtonActiveState(levelsButton, levelsAButton);
             setImage("bg/how-1.png");
-            controlsButton.setVisible(true);
-            controlsButton.setBounds(457, 126, 150, 41);
-            levelsButton.setVisible(true);
-            levelsButton.setBounds(645, 126, 121, 41);
-            entitiesButton.setVisible(true);
-            entitiesButton.setBounds(322, 126, 133, 41);
+            setButtonBounds();
         });
         controlsButton.addActionListener(e -> {
+            setButtonActiveState(controlsAButton, controlsButton);
+            setButtonActiveState(entitiesButton, entitiesAButton);
+            setButtonActiveState(levelsButton, levelsAButton);
             setImage("bg/how-2.png");
-            controlsButton.setVisible(true);
-            controlsButton.setBounds(457, 126, 150, 41);
-            levelsButton.setVisible(true);
-            levelsButton.setBounds(645, 126, 121, 41);
-            entitiesButton.setVisible(true);
-            entitiesButton.setBounds(322, 126, 133, 41);
+            setButtonBounds();
         });
         levelsButton.addActionListener(e -> {
+            setButtonActiveState(controlsButton, controlsAButton);
+            setButtonActiveState(entitiesButton, entitiesAButton);
+            setButtonActiveState(levelsAButton, levelsButton);
             setImage("bg/how-3.png");
-            controlsButton.setVisible(true);
-            controlsButton.setBounds(457, 126, 150, 41);
-            levelsButton.setVisible(true);
-            levelsButton.setBounds(645, 126, 121, 41);
-            entitiesButton.setVisible(true);
-            entitiesButton.setBounds(322, 126, 133, 41);
+            setButtonBounds();
         });
+    }
+
+    private void setButtonActiveState(ImageButton visibleButton, ImageButton hideButton) {
+        visibleButton.setVisible(true);
+        hideButton.setVisible(false);
+    }
+    private void setButtonBounds() {
+        entitiesButton.setBounds(330, 126, 133, 41);
+        controlsButton.setBounds(486, 126, 150, 41);
+        levelsButton.setBounds(655, 126, 121, 41);
+        entitiesAButton.setBounds(330, 126, 133, 41);
+        controlsAButton.setBounds(486, 126, 150, 41);
+        levelsAButton.setBounds(655, 126, 121, 41);
     }
 
     public void musicClick() {
